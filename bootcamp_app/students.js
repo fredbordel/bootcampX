b4bf0626ef89744d840deeb6e2528653eb14d382
vagrant <frederique.bordeleau@gmail.com>
2020-02-06 01:19:40 +0000

    parameterized query

--- bootcamp_app/students.js
+++ bootcamp_app/students.js
@@ -1,5 +1,16 @@
 const { Pool } = require('pg');
-const terminalArgs = process.argv.slice(2);
+
+const queryString = `
+  SELECT students.id as student_id, students.name as name, cohorts.name as cohort
+  FROM students
+  JOIN cohorts ON cohorts.id = cohort_id
+  WHERE cohorts.name LIKE $1
+  LIMIT $2;
+  `;
+
+const cohortName = process.argv[2];
+const limit = process.argv[3] || 5;
+const values = [`%${cohortName}%`, limit];
 
 const pool = new Pool({
   user: 'vagrant',
@@ -8,13 +19,7 @@ const pool = new Pool({
   database: 'bootcampx'
 });
 
-pool.query(`
-SELECT students.id as student_id, students.name as name, cohorts.name as cohort
-FROM students
-JOIN cohorts ON cohorts.id = cohort_id
-WHERE cohorts.name LIKE '%${terminalArgs[0]}%'
-LIMIT ${terminalArgs[1]};
-`)
+pool.query(queryString, values)
 .then(res => {
   res.rows.forEach(user => {
     console.log(`${user.name} has an id of ${user.student_id} and was in the ${user.cohort} cohort`);
--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -1,5 +1,16 @@
 const { Pool } = require('pg');
-const terminalArgs = process.argv.slice(2);
+const queryString = `
+SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
+FROM teachers
+JOIN assistance_requests ON teacher_id = teachers.id
+JOIN students ON student_id = students.id
+JOIN cohorts ON cohort_id = cohorts.id
+WHERE cohorts.name LIKE $1
+ORDER BY teacher;
+`;
+
+const cohortName = process.argv[2];
+const values = [`%${cohortName}%`];
 
 const pool = new Pool({
   user: 'vagrant',
@@ -8,15 +19,7 @@ const pool = new Pool({
   database: 'bootcampx'
 });
 
-pool.query(`
-SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
-FROM teachers
-JOIN assistance_requests ON teacher_id = teachers.id
-JOIN students ON student_id = students.id
-JOIN cohorts ON cohort_id = cohorts.id
-WHERE cohorts.name = '${process.argv[2] || 'JUL02'}'
-ORDER BY teacher;
-`)
+pool.query(queryString, values)
 .then(res => {
   res.rows.forEach(row => {
     console.log(`${row.cohort}: ${row.teacher}`);
